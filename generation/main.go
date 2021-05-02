d7a7dc53492f6637254c153e235430af63b73946
Andrew Regan <aregan@gmail.com>
2021-05-02 21:34:38 +0100

    Switch away from x/tools/go/analysis

--- generation/main.go
+++ generation/main.go
@@ -1,7 +1,6 @@
 package generation
 
 import (
-	"errors"
 	"fmt"
 	"go/ast"
 	"log"
@@ -9,138 +8,168 @@ import (
 	"strings"
 	"text/template"
 
-	"golang.org/x/tools/go/analysis"
-	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
+	"golang.org/x/tools/go/packages"
 )
-
-var DashboardGenerator = &analysis.Analyzer{
-	Name:     "promenadeGrafanaDashboardGenerator",
-	Doc:      "Generates Grafana Dashboards from Promenade (Prometheus) metrics via static",
-	Requires: []*analysis.Analyzer{inspect.Analyzer},
-	Run:      run,
-}
 
 const PromenadePkg = "github.com/poblish/promenade/api.PrometheusMetrics"
 
-func run(pass *analysis.Pass) (interface{}, error) {
-	fmt.Println(">>> Starting run", pass.IgnoredFiles)
+type DashboardGenerator struct {
+	currentMetricPrefix      string
+	caseSensitiveMetricNames bool
+	alreadyGotErrors         bool
+	foundMetricsObject       bool
+}
 
-	inspect, ok := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
-	// fmt.Println(reflect.TypeOf(inspect))
-	if !ok {
-		return nil, errors.New("analyzer is not type *inspector.Inspector")
-	}
-
+func (dg *DashboardGenerator) Run(loadedPkgs []*packages.Package) error {
 	nodeFilter := []ast.Node{(*ast.CallExpr)(nil)}
 
-	currentMetricPrefix := ""
-	caseSensitiveMetricNames := false
+	metrics := []*metric{}
 
-	metrics := []metric{}
+	dg.currentMetricPrefix = ""
+	dg.caseSensitiveMetricNames = false
+	dg.alreadyGotErrors = false
+	dg.foundMetricsObject = false
 
-	dashboardColumnPosition := 0
-	alreadyGotErrors := false
+	for _, eachPkg := range loadedPkgs {
+		fmt.Println(">> Examining", eachPkg.PkgPath)
 
-	inspect.Preorder(nodeFilter, func(node ast.Node) {
-		switch stmt := node.(type) {
-		case *ast.CallExpr:
-			if mthd, ok := stmt.Fun.(*ast.SelectorExpr); ok {
-				if ident, ok := mthd.X.(*ast.Ident); ok {
-					typeName := pass.TypesInfo.Uses[ident].Type().String()
+		inspector.New(eachPkg.Syntax).Preorder(nodeFilter, func(node ast.Node) {
+			switch stmt := node.(type) {
+			case *ast.CallExpr:
 
-					// Don't do == on type in case of pointer prefix
-					if strings.HasSuffix(typeName, PromenadePkg) && mthd.Sel.Name != "TestHelper" {
-						metricCall := mthd.Sel.Name
-						metricName := stripQuotes(stmt.Args[0].(*ast.BasicLit).Value)
+				if mthd, ok := stmt.Fun.(*ast.SelectorExpr); ok {
 
-						var fullMetricName string
-						if caseSensitiveMetricNames {
-							fullMetricName = currentMetricPrefix + normalizer.Replace(metricName)
-						} else {
-							fullMetricName = currentMetricPrefix + normaliseAndLowercaseName(metricName)
+					if ident, ok := mthd.X.(*ast.Ident); ok {
+						typeName := eachPkg.TypesInfo.Uses[ident].Type().String()
 
-						}
+						// Don't do == on type in case of pointer prefix
+						if strings.HasSuffix(typeName, PromenadePkg) && mthd.Sel.Name != "TestHelper" {
+							metricName := stripQuotes(stmt.Args[0].(*ast.BasicLit).Value)
 
-						// fmt.Println(metricCall, metricName, "=>", fullMetricName)
-
-						metricType := ""
-						if strings.HasPrefix(metricCall, "Counter") { // FIXME Parse arg #1 of CounterWithLabel (string) & CounterWithLabels ([]string)
-							metricType = "counter"
-						} else if strings.HasPrefix(metricCall, "Error") {
-							if alreadyGotErrors {
-								return
+							newMetric := dg.interceptMetric(mthd.Sel.Name, metricName)
+							if newMetric != nil {
+								metrics = append(metrics, newMetric)
 							}
 
-							alreadyGotErrors = true
-							metricType = "errors"
+						} else {
+							statementType := eachPkg.TypesInfo.Types[stmt].Type.String()
 
-						} else if strings.HasPrefix(metricCall, "Gauge") {
-							metricType = "gauge"
-						} else if strings.HasPrefix(metricCall, "Histo") {
-							metricType = "histogram"
-						} else if strings.HasPrefix(metricCall, "Summary") || strings.HasPrefix(metricCall, "Timer") {
-							metricType = "gauge"
-						}
+							if mthd.Sel.Name == "NewMetrics" && statementType == PromenadePkg {
 
-						metrics = append(metrics, metric{PanelColumn: dashboardColumnPosition, MetricsPrefix: currentMetricPrefix,
-							FullMetricName: fullMetricName, PanelTitle: metricName, MetricType: metricType})
+								dg.foundMetricsObject = true
 
-						dashboardColumnPosition = 12 - dashboardColumnPosition // of 24
+								rawMetricPrefix := ""
+								rawPrefixSeparator := "_" // as per Prometheus lib standard
 
-					} else {
-						statementType := pass.TypesInfo.Types[stmt].Type.String()
-						if mthd.Sel.Name == "NewMetrics" && statementType == PromenadePkg {
-
-							rawMetricPrefix := ""
-							rawPrefixSeparator := "_" // as per Prometheus lib standard
-
-							// Parse the single argument to NewMetrics, deconstruct the Opts
-							for _, elt := range stmt.Args[0].(*ast.CompositeLit).Elts {
-								if kv, ok := elt.(*ast.KeyValueExpr); ok {
-									switch kv.Key.(*ast.Ident).Name {
-									case "MetricNamePrefix":
-										rawMetricPrefix = stripQuotes(kv.Value.(*ast.BasicLit).Value)
-									case "PrefixSeparator":
-										rawPrefixSeparator = stripQuotes(kv.Value.(*ast.BasicLit).Value)
-									case "CaseSensitiveMetricNames":
-										caseSensitiveMetricNames = true
+								// Parse the single argument to NewMetrics, deconstruct the Opts
+								for _, elt := range stmt.Args[0].(*ast.CompositeLit).Elts {
+									if kv, ok := elt.(*ast.KeyValueExpr); ok {
+										switch kv.Key.(*ast.Ident).Name {
+										case "MetricNamePrefix":
+											rawMetricPrefix = stripQuotes(kv.Value.(*ast.BasicLit).Value)
+										case "PrefixSeparator":
+											rawPrefixSeparator = stripQuotes(kv.Value.(*ast.BasicLit).Value)
+										case "CaseSensitiveMetricNames":
+											dg.caseSensitiveMetricNames = true
+										}
 									}
 								}
-							}
 
-							currentMetricPrefix = normaliseAndLowercaseName(rawMetricPrefix)
-							if currentMetricPrefix != "" && !strings.HasSuffix(currentMetricPrefix, rawPrefixSeparator) {
-								currentMetricPrefix += rawPrefixSeparator
+								dg.currentMetricPrefix = normaliseAndLowercaseName(rawMetricPrefix)
+								if dg.currentMetricPrefix != "" && !strings.HasSuffix(dg.currentMetricPrefix, rawPrefixSeparator) {
+									dg.currentMetricPrefix += rawPrefixSeparator
+								}
 							}
+						}
+					} else if subExpr, ok := mthd.X.(*ast.SelectorExpr); ok /* Nested calls like `defer x.Timer()` */ {
+						subExprTypeName := eachPkg.TypesInfo.Types[subExpr].Type.String()
 
-							// fmt.Println("> NewMetrics", currentMetricPrefix)
+						if strings.HasSuffix(subExprTypeName, PromenadePkg) && mthd.Sel.Name != "TestHelper" {
+							metricName := stripQuotes(stmt.Args[0].(*ast.BasicLit).Value)
+
+							newMetric := dg.interceptMetric(mthd.Sel.Name, metricName)
+							if newMetric != nil {
+								metrics = append(metrics, newMetric)
+							}
 						}
 					}
 				}
 			}
-		}
-	})
+		})
+	}
 
-	fmt.Println("<<< Outputting...", metrics)
+	if !dg.foundMetricsObject {
+		log.Fatalf("ERROR: No Metrics found")
+	}
+
+	if len(metrics) < 1 {
+		log.Fatalf("No Promenade metrics found")
+	}
+
+	// Complete...
+	dashboardColumnPosition := 0
+
+	for _, eachMetric := range metrics {
+		eachMetric.MetricsPrefix = dg.currentMetricPrefix
+		eachMetric.FullMetricName = dg.currentMetricPrefix + eachMetric.normalisedMetricName
+		eachMetric.PanelColumn = dashboardColumnPosition
+
+		dashboardColumnPosition = 12 - dashboardColumnPosition // of 24
+
+		fmt.Println(eachMetric.metricCall, "=>", eachMetric.FullMetricName)
+	}
 
 	tmpl, _ := template.New("default").Parse(DefaultDashboardTemplate)
 	// tmpl := template.Must(template.ParseGlob("/Users/andrewregan/Development/Go\\ work/promenade/templates/dashboard.json"))
-	err := tmpl.Execute(os.Stdout, &dashboardData{Metrics: metrics, Title: "MyTitle", Id: "MyId"})
-	if err != nil {
-		log.Fatalf("template execution: %s", err)
+	tErr := tmpl.Execute(os.Stdout, &dashboardData{Metrics: metrics, Title: "MyTitle", Id: "MyId"})
+	if tErr != nil {
+		log.Fatalf("template execution: %s", tErr)
 	}
 
-	return nil, nil
+	return tErr
+}
+
+func (dg *DashboardGenerator) interceptMetric(metricCall string, metricName string) *metric {
+	var normalisedMetricName string
+	if dg.caseSensitiveMetricNames {
+		normalisedMetricName = normalizer.Replace(metricName)
+	} else {
+		normalisedMetricName = normaliseAndLowercaseName(metricName)
+	}
+
+	metricType := ""
+	if strings.HasPrefix(metricCall, "Counter") { // FIXME Parse arg #1 of CounterWithLabel (string) & CounterWithLabels ([]string)
+		metricType = "counter"
+	} else if strings.HasPrefix(metricCall, "Error") {
+		if dg.alreadyGotErrors {
+			return nil
+		}
+
+		dg.alreadyGotErrors = true
+		metricType = "errors"
+
+	} else if strings.HasPrefix(metricCall, "Gauge") {
+		metricType = "gauge"
+	} else if strings.HasPrefix(metricCall, "Histo") {
+		metricType = "histogram"
+	} else if strings.HasPrefix(metricCall, "Summary") || strings.HasPrefix(metricCall, "Timer") {
+		metricType = "gauge"
+	}
+
+	return &metric{metricCall: metricCall, normalisedMetricName: normalisedMetricName, PanelTitle: metricName, MetricType: metricType}
 }
 
 type dashboardData struct {
-	Metrics []metric
+	Metrics []*metric
 	Title   string
 	Id      string
 }
 
 type metric struct {
+	metricCall           string
+	normalisedMetricName string
+
 	PanelColumn    int
 	MetricsPrefix  string
 	MetricType     string
--- main.go
+++ main.go
@@ -1,10 +1,43 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"github.com/poblish/boulevard/generation"
-	"golang.org/x/tools/go/analysis/singlechecker"
+	"golang.org/x/tools/go/packages"
 )
 
+type packagesList []string
+
+var packageFlags packagesList
+
 func main() {
-	singlechecker.Main(generation.DashboardGenerator)
+	flag.Var(&packageFlags, "pkg", "Packages to scan")
+	flag.Parse()
+
+	conf := packages.Config{
+		Mode:  packages.NeedName | packages.NeedTypes | packages.NeedSyntax | packages.NeedTypesInfo,
+		Tests: false,
+	}
+
+	loadedPkgs, err := packages.Load(&conf, packageFlags...)
+	if err != nil {
+		log.Fatalf("Could not load packages %s", err)
+	}
+
+	generator := &generation.DashboardGenerator{}
+	err = generator.Run(loadedPkgs)
+	if err != nil {
+		log.Fatalf("Run failed %s", err)
+	}
+}
+
+func (i *packagesList) String() string {
+	return "my string representation"
+}
+
+func (i *packagesList) Set(value string) error {
+	*i = append(*i, value)
+	return nil
 }
